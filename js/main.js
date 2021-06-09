f1158395885793028d69eded5a240c76090fa2f3
DESKTOP-7VDISKD\mayum <mayumi.furukawa.info@gmail.com>
2021-06-09 13:15:24 +0900

    スムーススクロール、ボタン全てを選択範囲にするなど。

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 console.log('hello');
+$(function() {
+    // TOPに戻るボタン
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function() {
+        var w = $(window).width();
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スムーススクロールでページトップへ
+    topBtn.click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
