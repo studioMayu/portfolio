d3bcc39b6790c135b7b752604f47fe69f1919419
Mayu <71933177+studioMayu@users.noreply.github.com>
2024-07-10 18:12:12 +0900

    Add files via upload

---
+++ ajisai_gallery/assets/js/app.js
@@ -0,0 +1,10 @@
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+new Oreroll("[data-trigger]", { rootMargin: "0px 0px -30%" });
+new Oreroll("[data-scrub]", { once: false, scrub: true });
