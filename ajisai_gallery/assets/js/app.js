404ded02553cd2efc641f849562627a69fc0acc0
Mayu <71933177+studioMayu@users.noreply.github.com>
2024-09-08 21:58:11 +0900

    Update app.js

--- ajisai_gallery/assets/js/app.js
+++ ajisai_gallery/assets/js/app.js
@@ -7,4 +7,19 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 new Oreroll("[data-trigger]", { rootMargin: "0px 0px -30%" });
-new Oreroll("[data-scrub]", { once: false, scrub: true });
+new Oreroll("[data-scrub]", { once: true, scrub: true });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+      }
+    });
+  });
+
+  // .h2 クラスを持つ要素を監視
+  document.querySelectorAll('.h2').forEach(element => {
+    observer.observe(element);
+  });
+});
